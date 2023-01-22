76ff4dc1e353f7bb9c0d5404675f2a33e5491900
TheUltiOne <118567564+warden161@users.noreply.github.com>
2023-01-22 16:30:39 -0500

    Main file improvement

--- BetterSinkholes2/BetterSinkholes2.cs
+++ BetterSinkholes2/BetterSinkholes2.cs
@@ -3,11 +3,17 @@ using Exiled.Events.EventArgs.Player;
 using Hazards;
 using UnityEngine;
 using CustomPlayerEffects;
+using System;
 
 namespace BetterSinkholes2
 {
     public class BetterSinkholes2 : Plugin<Config>
     {
+        public override string Author { get; } = "warden161 (original by blackruby)";
+        public override string Name { get; } = "BetterSinkholes2";
+        public override Version Version { get; } = new(1, 1, 0);
+        public override Version RequiredExiledVersion { get; } = new(6, 0, 0);
+
         public override void OnEnabled()
         {
             Exiled.Events.Handlers.Player.StayingOnEnvironmentalHazard += OnStayingOnSinkhole;
